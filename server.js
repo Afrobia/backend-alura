00dc10259902815dc41682db5447a5e34f05178d
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-11-21 11:05:56 -0300

    implementado get para ecesso a na base de dados mockada

--- server.js
+++ server.js
@@ -1,10 +1,54 @@
-import express from 'express';
+import express from "express";
+
+const posts = [
+  {
+    id: 1,
+    descricao: "Foto teste",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    descricao: "Gato fazendo yoga",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 3,
+    descricao: "Gatinho com bola de lã",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 4,
+    descricao: "Gato curioso olhando pela janela",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 5,
+    descricao: "Gato ronronando no sol",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 6,
+    descricao: "Gato comendo ração",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+];
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
-    console.log("Servidor escutando...")
+  console.log("Servidor escutando...");
 });
 
-app.get(("/"), (req, res) => {
-    res.status(200).send("Boas Vindas a imersão back-end da Alura");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function buscarPostPorId(id) {
+    return posts.findIndex((post) => post.id === Number(id));
+};
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorId(req.params.id);
+  res.status(200).json(posts[index]);
 });
