0ee5c143d0141e39fc970511b8c4b3c32ea2b225
Beatriz Santana <beatrizsantana1993@gmail.com>
2024-11-21 16:57:26 -0300

    estabelecendo conectividade com o db

--- server.js
+++ server.js
@@ -1,37 +1,14 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
 
-const posts = [
-  {
-    id: 1,
-    descricao: "Foto teste",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    descricao: "Gato fazendo yoga",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 3,
-    descricao: "Gatinho com bola de lã",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 4,
-    descricao: "Gato curioso olhando pela janela",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 5,
-    descricao: "Gato ronronando no sol",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 6,
-    descricao: "Gato comendo ração",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-];
+const conexao = await conectarAoBanco(process.env.DB_URL);
+
+async function getAllPosts() {
+    const db = await conexao.db("imersao-instabyte");
+    const colecao = db.collection("posts");
+
+    return await colecao.find().toArray();
+};
 
 const app = express();
 app.use(express.json());
@@ -40,16 +17,9 @@ app.listen(3000, () => {
   console.log("Servidor escutando...");
 });
 
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
+app.get("/posts", async (req, res) => {
+  const result = await getAllPosts()
+  res.status(200).json(result);
 });
 
-function buscarPostPorId(id) {
-    return posts.findIndex((post) => post.id === Number(id));
-};
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id);
-  res.status(200).json(posts[index]);
-});
 
